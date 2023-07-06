abfa0a2fe936775aa197551e3cf741ebb2984754
Korina K <korinakatsa@gmail.com>
2023-07-06 10:58:56 +0200

    hii

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 provider "aws" {
   region = "us-west-2"
 }
-resource "aws_s3_bucket" "dev" {
+/*resource "aws_s3_bucket" "dev" {
   bucket = "terraformbucket756"
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
-}
+}*/
 resource "aws_instance" "deham6demos"{
     ami = "ami-0fa167c2af1ea0840"
     instance_type = "t2.micro"
-    key_name = "deham630062023"
-    vpc_security_group_ids = ["sg-0668745a1e96b9e55"]
-    subnet_id = "subnet-05527afe89ba4d0ee"
+    key_name = "vockey"
+    vpc_security_group_ids = ["sg-0fb6532a814a34799"]
+    subnet_id = "subnet-0f2548ccc3ccc9090"
     tags = {
         Name = "deham6demos"
     }
